ea810ef02fe2db8048a8483fd3c69d06272f5b3b
abravo31 <102750700+abravo31@users.noreply.github.com>
2023-12-06 17:57:37 +0000

    now i have every method that works, we need to connect it the true database postgre

--- index.js
+++ index.js
@@ -12,15 +12,53 @@ app.get('/users', (req,res) => {
     res.status(200).json(users)
 });
 
-// app.get('/users/:id', (req, res) => {
-//     const id = parseInt( req.params.id );
-//     const users = users.find(users => users.id === id);
-//     res.status(200).json(users)
-// });
+app.get('/users/:id', (req, res) => {
+    const id = parseInt( req.params.id );
+    const user = users.find(user => user.id === id);
 
+    // Manage if the user isnot find 
+    if (!user) {
+        return res.status(404).send('User not found');
+    }
+
+    res.status(200).json(user);
+});
+
+// To create a new user with POST
+app.post('/users', (req, res) => {
+    users.push(req.body);
+    res.status(200).json(users);
+});
+
+app.put('/users/:id', (req,res) => {    
+    const id = parseInt(req.params.id);  
+    let userIndex = users.findIndex(user => user.id === id);
+
+    if (userIndex === -1) {
+        return res.status(404).send('User not found');
+    }
+
+    users[userIndex] = {
+        ...users[userIndex],
+        ...req.body
+    }
+    
+    res.status(200).json(users[userIndex]);
+});
+
+app.delete('/users/:id', (req,res) => {    
+    const id = parseInt(req.params.id);    
+    const userIndex = users.findIndex(user => user.id === id);
+
+    if (userIndex === -1) {
+        return res.status(404).send('User not found');
+    }
+
+    users.splice(userIndex, 1);
+    res.status(200).json(users)
+});
 
 // The server can listen the choosed port
 const PORT = 8000;
 app.listen(PORT, () => { 
     console.log('Server is listen') });
-
