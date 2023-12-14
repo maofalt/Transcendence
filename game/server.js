d2680a1aa685e538c018b90bab597b8cea0f6e74
znogueir <zachary.nogueira@gmail.com>
2023-12-14 15:58:23 +0100

    new branch zach, removed amandas file for safety, first push

--- game/server.js
+++ game/server.js
@@ -3,7 +3,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public')); // Assurez-vous que vos fichiers HTML/JS/CSS sont dans un dossier 'public'
+app.use(express.static('public/remote/')); // Assurez-vous que vos fichiers HTML/JS/CSS sont dans un dossier 'public'
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
