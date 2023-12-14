f86329e607f49c79cd03e289e86db4517c9c2a4f
Yoel RIDGWAY-LOPEZ <yoelridgway@gmail.com>
2023-12-14 18:51:12 +0100

    added background

--- srcs/requirements/front-end/react-app/js-srcs/App.jsx
+++ srcs/requirements/front-end/react-app/js-srcs/App.jsx
@@ -11,7 +11,7 @@ function App() {
 	return (
 		<Router>
 			<div className='App'>
-				<Navbar />
+				{/* <Navbar /> */}
 				<div className="content">
 					<Routes>
 						<Route path='/' element={<Home jsonUrl={jsonUrl}/>} />
--- srcs/requirements/front-end/react-app/js-srcs/Home.jsx
+++ srcs/requirements/front-end/react-app/js-srcs/Home.jsx
@@ -16,10 +16,12 @@ const Home = ({ jsonUrl }) => {
 	// console.log(data);
 
 	return (
-		<div className="menu">
-			<Link to="/game"><h1 className='menuItem play'>{ play }</h1></Link>
-			<Link to="/profile"><h1 className='menuItem profile'>{ profile }</h1></Link>
-			<Link to="/options"><h1 className='menuItem options'>{ options }</h1></Link>
+		<div className="homepage">
+			<div className="menu">
+				<Link to="/game"><h1 className='menuItem play'>{ play }</h1></Link>
+				<Link to="/profile"><h1 className='menuItem profile'>{ profile }</h1></Link>
+				<Link to="/options"><h1 className='menuItem options'>{ options }</h1></Link>
+			</div>
 		</div>
 	);
 }
