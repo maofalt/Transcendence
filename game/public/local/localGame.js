63d0bac7b465017d20d65c86e88837a9ae658eb7
znogueir <zachary.nogueira@gmail.com>
2023-12-15 16:51:03 +0100

    local pong working, missing score board and and collisions between walls and paddles

--- game/public/local/localGame.js
+++ game/public/local/localGame.js
@@ -9,29 +9,32 @@ console.log("testtest");
 const ball = {
 	x: canvas.width / 2,
 	y: 50,
-	spX: 2,
-	spY: 2,
-	r: 10,
+	vX: 1,
+	vY: 1,
+	r: 8,
+	sp: 4,
 	color: "#FFFFFF"
 };
 
 const paddle1 = {
 	x: 0,
 	y: 0,
-	spX: 0,
-	spY: 0,
-	width: 20,
+	vX: 0,
+	vY: 0,
+	width: 15,
 	height: 70,
+	sp: 6,
 	color: "#0000FF"
 }
 
 const paddle2 = {
 	x: 0,
 	y: 0,
-	spX: 0,
-	spY: 0,
-	width: 20,
+	vX: 0,
+	vY: 0,
+	width: 15,
 	height: 70,
+	sp: 6,
 	color: "#FF0000"
 }
 
@@ -41,6 +44,7 @@ paddle1.y = (canvas.height - paddle1.height) / 2;
 paddle2.x = canvas.width - (paddle2.width * 2);
 paddle2.y = (canvas.height - paddle2.height) / 2;
 
+// players
 const player1 = {
 	login: "Player 1",
 	paddle: paddle1,
@@ -53,20 +57,36 @@ const player2 = {
 	score: 0
 }
 
+// vector calculations for ball dir
+function normalizeBallDir() {
+	l = Math.sqrt(ball.vX * ball.vX + ball.vY * ball.vY);
+	ball.vX /= l;
+	ball.vY /= l;
+	ball.vX *= ball.sp;
+	ball.vY *= ball.sp;
+}
+
+function getRandomDir() {
+	let signX = Math.random();
+	let signY = Math.random();
+	ball.vX = (Math.random() + 1) * ((signX >= 0.5) ? 1 : -1);
+	ball.vY = (Math.random() + 1) * ((signY >= 0.5) ? 1 : -1);
+	normalizeBallDir();
+}
+
 // init board
 function initBoard() {
 	ball.x = canvas.width / 2;
 	ball.y = 50;
-	ball.spX = 2;
-	ball.spY = 2;
+	getRandomDir();
 	paddle1.x = paddle1.width;
 	paddle1.y = (canvas.height - paddle1.height) / 2;
-	paddle1.spX = 0;
-	paddle1.spY = 0;
+	paddle1.vX = 0;
+	paddle1.vY = 0;
 	paddle2.x = canvas.width - (paddle2.width * 2);
 	paddle2.y = (canvas.height - paddle2.height) / 2;
-	paddle2.spX = 0;
-	paddle2.spY = 0;
+	paddle2.vX = 0;
+	paddle2.vY = 0;
 }
 
 // drawing objects
@@ -86,25 +106,45 @@ function drawPaddle(paddle) {
 	ctx.closePath();
 }
 
-// collisions
+function calculateBallDir(paddleNbr) {
+	let contactX = paddle1.x + paddle1.width;
+	let contactY = ball.y;
+	let paddleCenterX = paddle1.x;
+	let paddleCenterY = paddle1.y + paddle1.height / 2;
+
+	if (paddleNbr == 2) {
+		contactX = paddle2.x;
+		contactY = ball.y;
+		paddleCenterX = paddle2.x + paddle2.width;
+		paddleCenterY = paddle2.y + paddle2.height / 2;
+	}
+
+	let l = 0;
+
+	ball.vX = contactX - paddleCenterX;
+	ball.vY = contactY - paddleCenterY;
+	normalizeBallDir();
+}
+
 function ballHitsWall() {
 	if (ball.y + ball.r >= canvas.height || ball.y - ball.r <= 0)
-		ball.spY *= -1;
+		ball.vY *= -1;
 }
 
 function ballHitsPaddle1() {
-	if (ball.y >= paddle1.y && ball.y <= paddle1.y + paddle1.height && 
-		(ball.x - ball.r <= paddle1.x + paddle1.width)) {
-		ball.spX *= -1;
-		ball.spY += paddle1.spY;
+	if (ball.y >= paddle1.y && ball.y <= paddle1.y + paddle1.height) {
+		if (ball.x > paddle1.x + paddle1.width && ball.x - ball.r <= paddle1.x + paddle1.width) {
+			calculateBallDir(1);
+		}
 	}
 }
 
 function ballHitsPaddle2() {
-	if (ball.y >= paddle2.y && ball.y <= paddle2.y + paddle2.height && 
-		(ball.x + ball.r >= paddle2.x)) {
-		ball.spX *= -1;
-		ball.spY += paddle2.spY;
+	if (ball.y >= paddle2.y && ball.y <= paddle2.y + paddle2.height) {
+		if (ball.x < paddle2.x && ball.x + ball.r >= paddle2.x) {
+			ball.vX *= -1;
+			calculateBallDir(2);
+		}
 	}
 }
 
@@ -122,19 +162,20 @@ function updateBall() {
 		if (player1.score >= 10 || player2.score >= 10)
 			return true;
 		initBoard();
-		startRound();
+		// startRound();
 	}
 	ballHitsWall();
 	ballHitsPaddle1();
 	ballHitsPaddle2();
-	ball.x += ball.spX;
-	ball.y += ball.spY;
+	ball.x += ball.vX;
+	ball.y += ball.vY;
+	// console.log("update ball over");
 	return false;
 }
 
 function updatePaddle(paddle) {
-	paddle.x += paddle.spX;
-	paddle.y += paddle.spY;
+	paddle.x += paddle.vX;
+	paddle.y += paddle.vY;
 }
 
 // controlling paddles
@@ -142,19 +183,19 @@ function handleKeyPress(event) {
 	switch (event.key) {
 		case "ArrowUp":
 			console.log("up");
-			paddle2.spY = -2;
+			paddle2.vY = -2;
 			break;
 		case "ArrowDown":
 			console.log("down");
-			paddle2.spY = 2;
+			paddle2.vY = 2;
 			break;
 		case "w":
 			console.log("w");
-			paddle1.spY = -2;
+			paddle1.vY = -2;
 			break;
 		case "s":
 			console.log("s");
-			paddle1.spY = 2;
+			paddle1.vY = 2;
 			break;
 	}
 }
@@ -163,20 +204,20 @@ function handleKeyRelease(event) {
 	switch (event.key) {
 		case "ArrowUp":
 		case "ArrowDown":
-			paddle2.spY = 0;
+			paddle2.vY = 0;
 			break;
 		case "w":
 		case "s":
-			paddle1.spY = 0;
+			paddle1.vY = 0;
 			break;
 	}
 }
 
 // starting round
-function startRound() {
-	ball.spX = 2;
-	ball.spY = 2;
-}
+// function startRound() {
+// 	ball.vX *= ball.sp;
+// 	ball.vY *= ball.sp;
+// }
 
 //rendering frame
 function renderFrame() {
@@ -185,9 +226,9 @@ function renderFrame() {
 	updatePaddle(paddle1);
 	updatePaddle(paddle2);
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawBall();
 	drawPaddle(player1.paddle);
 	drawPaddle(player2.paddle);
-	drawBall();
 }
 
 function endGame() {
@@ -199,6 +240,18 @@ function endGame() {
 document.addEventListener("keydown", handleKeyPress);
 document.addEventListener("keyup", handleKeyRelease);
 
-initBoard();
-startRound();
-let gameInterval = setInterval(renderFrame, 10);
+// function run() {
+	initBoard();
+	let gameInterval = setInterval(renderFrame, 10);
+// }
+
+// run();
+
+/*
+TO DO :
+	- add score board;
+	- add proper collisions i guess;
+	- add proper touches with paddle;
+	- add retry option ? infinite loop;
+*/
+
