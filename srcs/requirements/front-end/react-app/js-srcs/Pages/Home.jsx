193a6f05e31ad152788cb341878c1414634048db
Yoel <yoelridgway@gmail.com>
2023-12-16 20:40:27 +0100

    cleaned up code

--- srcs/requirements/front-end/react-app/js-srcs/Pages/Home.jsx
+++ srcs/requirements/front-end/react-app/js-srcs/Pages/Home.jsx
@@ -13,21 +13,9 @@ const Home = ({ jsonUrl }) => {
 	return (
 		<div className="homepage">
 			<div className="menu">
-				<div className="menu-item">
-					{/* <object className="rhombus left" type="image/svg+xml" data="assets/RhombusLeft.svg">Your browser does not support SVG</object> */}
-					<div className="ban"><Link to="/game"><GlowText text={ play } hoverGlow={ hoverGlow } rhombus={ rhombus }/></Link></div>
-					{/* <object className="rhombus right" type="image/svg+xml" data="assets/RhombusRight.svg">Your browser does not support SVG</object> */}
-				</div>
-				<div className="menu-item">
-					{/* <object className="rhombus left" type="image/svg+xml" data="assets/RhombusLeft.svg">Your browser does not support SVG</object> */}
-					<Link to="/profile"><GlowText text={ profile } hoverGlow={ hoverGlow } rhombus={ rhombus }/></Link>
-					{/* <object className="rhombus right" type="image/svg+xml" data="assets/RhombusRight.svg">Your browser does not support SVG</object> */}
-				</div>
-				<div className="menu-item">
-					{/* <object className="rhombus left" type="image/svg+xml" data="assets/RhombusLeft.svg">Your browser does not support SVG</object> */}
-					<Link to="/options"><GlowText text={ options } hoverGlow={ hoverGlow } rhombus={ rhombus }/></Link>
-					{/* <object className="rhombus right" type="image/svg+xml" data="assets/RhombusRight.svg">Your browser does not support SVG</object> */}
-				</div>
+				<Link to="/game"><GlowText text={ play } hoverGlow={ hoverGlow } rhombus={ rhombus }/></Link>
+				<Link to="/profile"><GlowText text={ profile } hoverGlow={ hoverGlow } rhombus={ rhombus }/></Link>
+				<Link to="/options"><GlowText text={ options } hoverGlow={ hoverGlow } rhombus={ rhombus }/></Link>
 			</div>
 		</div>
 	);
