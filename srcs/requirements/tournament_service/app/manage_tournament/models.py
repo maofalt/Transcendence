9aa69cfadd7dff652de06e543f49db754ebd780f
Amanda BRAVO <abysims31@gmail.com>
2024-01-07 18:22:52 +0100

    models created in django

--- srcs/requirements/tournament_service/app/manage_tournament/models.py
+++ srcs/requirements/tournament_service/app/manage_tournament/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Tournament(models.Model):
     tournament_id = models.AutoField(primary_key=True)
     tournement_name = models.CharField(max_length=255, unique=True)
@@ -9,9 +8,25 @@ class Tournament(models.Model):
     nbr_of_player = models.IntegerField(default=2)
     tournament_type = models.ForeignKey('TournamentType', on_delete=models.SET_NULL, null=True, to_field='type_id')
     registration = models.ForeignKey('RegistrationType', on_delete=models.SET_NULL, null=True, to_field='type_id')
-    settings_id = models.ForeignKey('MatchSetting', on_delete=models.SET_NULL, null=True)
-    end_date = models.DateField()
-    rules = models.TextField()
+    setting_id = models.ForeignKey('MatchSetting', on_delete=models.SET_NULL, null=True, to_field='setting_id')
+    registration_period_min = models.IntegerField(default=15)
+    host_id = models.IntegerField()
+
+class TournamentMatch(models.Model):
+    match_id = models.AutoField(primary_key=True)
+    tournament_id = models.ForeignKey('Tournament', on_delete=models.CASCADE, related_name='match' )
+    round_number = models.IntegerField()
+    match_time = models.DateTimeField(null=True) 
+    match_result = models.CharField(max_length=255)
+
+class MatchSetting(models.Model):
+    setting_id = models.AutoField(primary_key=True)
+    duration_sec = models.IntegerField(default=210)
+    max_score = models.IntegerField(default=5)
+    nbr_of_sets = models.IntegerField(default=1)
+    paddle_speed = models.IntegerField(default=10)
+    ball_speed = models.IntegerField(default=10)
+    nbr_of_players = models.IntegerField(default=2)
 
 class GameType(models.Model):
     type_id = models.AutoField(primary_key=True)
@@ -26,8 +41,8 @@ class RegistrationType(models.Model):
     type_name = models.CharField(max_length=255)
 
 class TournamentPlayer(models.Model):
-    tournament_id = models.ForeignKey('Tournament', on_delete=models.CASCADE)
-    player = models.ForeignKey('Player', on_delete=models.CASCADE)
+    tournament_id = models.ForeignKey('Tournament', on_delete=models.CASCADE, related_name='tournament')
+    player = models.ForeignKey('Player', on_delete=models.CASCADE, related_name='player')
 
     class Meta:
         unique_together = ('tournament_id', 'player')
@@ -36,17 +51,11 @@ class Player(models.Model):
     player_id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=255)
 
+    class Meta:
+        unique_together = ('player_id', 'username')
 
-
-CREATE TABLE "tournaments_list" (
-#   "tournament_id" integer PRIMARY KEY,
-#   "game_type" integer,
-#   "tournament_name" varchar(255) UNIQUE NOT NULL,
-#   "created_at" timestamp DEFAULT (now()),
-#   "nbr_of_player" integer NOT NULL DEFAULT 2,
-#   "tournament_type_id" integer,
-#   "registration" integer,
-#   "setting_id" integer,
-  "registration_period_min" integer NOT NULL DEFAULT 15,
-  "host_id" integer NOT NULL
-);
+class MatchParticipants(models.Model):
+    match_id = models.ForeignKey('TournamentMatch', on_delete=models.CASCADE, related_name='participants')
+    player_id = models.ForeignKey('Player', on_delete=models.CASCADE, related_name='playeridfrommatch')
+    is_winner = models.BooleanField(default=False)
+    participant_sacore = models.IntegerField(default=0)
