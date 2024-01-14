360d8863a086779e4c9e6d10d04f2aa2175cb51d
yoel <yridgway@student.42.fr>
2024-01-14 19:09:03 +0100

    refactorred router() to routeTo() and changed the object type of the routes obj to contain more useful information

--- srcs/requirements/front-end/vanilla/js/main.js
+++ srcs/requirements/front-end/vanilla/js/main.js
@@ -1,4 +1,4 @@
-// import '@css/style.css'
+import '@css/style.css'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap';
 import javascriptLogo from '@public/javascript.svg';
@@ -11,53 +11,53 @@ import Game from '@views/Game.js';
 import Login from '@views/Login.js';
 import NotFound from '@views/NotFound.js';
 
-const navigateTo = url => {
-	// history.pushState(null, null, url);
-	console.log(url);
-	router();
-}
-
 const routes = {
 	'/': {
 		path: '/',
 		view: Home,
-		title: 'Pongyverse'
+		title: 'Pongyverse',
+		link: 'Home'
 	},
 	'/play': {
 		path: '/play',
 		view: Game,
-		title: 'Game'
+		title: 'Game',
+		link: 'Play'
 	},
 	'/game': {
 		path: '/game',
 		view: Game,
-		title: 'Game'
+		title: 'Game',
+		link: 'Play'
 	},
 	'/tournament': {
 		path: '/tournament',
 		view: Tournament,
-		title: 'Tournament'
+		title: 'Tournament',
+		link: 'Tournament'
 	},
 	'/options': {
 		path: '/options',
 		view: Options,
-		title: 'Options'
+		title: 'Options',
+		link: 'Options'
 	},
 	'/login': {
 		path: '/login',
-		view: Login,	// history.pushState(null, null, url);
-
-		title: 'Login'
+		view: Login,
+		title: 'Login',
+		link: 'Login'
 	},
 	'/404': {
 		path: '/404',
 		view: NotFound,
-		title: '404 Not Found'
+		title: '404 Not Found',
+		link: 'Not Found Page'
 	}
 };
 
 
-const router = async (url) => {
+const routeTo = async (url) => {
 	const urlobj = new URL(url); // get the current path
 	const path = urlobj.pathname; // get the current path
 	const match = routes[Object.keys(routes).find(route => route == path)] || routes['/404']; // find the matching route or use the 404 route
@@ -73,7 +73,7 @@ const router = async (url) => {
 };
 
 // listen for back and forward button clicks and route to the correct page
-window.addEventListener("popstate", router);
+window.addEventListener("popstate", () => routeTo(document.URL));
 
 document.addEventListener('DOMContentLoaded', () => {
 	// listen for clicks on html elements with nav-link property and navigate to them without refreshing
@@ -81,10 +81,10 @@ document.addEventListener('DOMContentLoaded', () => {
 		if (event.target.matches('[nav-link]')) {
 			event.preventDefault(); // prevent page refresh
 			if (event.target.href != document.URL) // only navigate if it goes to a new page
-				router(event.target.href);
+				routeTo(event.target.href);
 		}
 	});
-	router(document.URL); // route to page on load
+	routeTo(document.URL); // route to page on load
 });
 
 // Create a parent element
@@ -92,31 +92,27 @@ const parentElement = document.querySelector('#app');
 
 const elems = [];
 
-
 // Create individual child elements
-const h1Element = document.createElement('h1');
-h1Element.textContent = 'Hello World!';
-elems.push(h1Element);
+// const h1Element = document.createElement('h1');
+// h1Element.textContent = 'Hello World!';
+// elems.push(h1Element);
 
-const validRoutes = ['/', '/play', '/game', '/tournament', '/options', '/login'];
-Object.keys(routes).forEach(route => {
+const validRoutes = ['/', '/play', '/tournament', '/options', '/login'];
+Object.entries(routes).forEach(([route, view]) => {
 	if (validRoutes.includes(route)) {
 		const link = document.createElement('a');
 		link.href = route;
 		link.classList.add('nav-link');
 		link.setAttribute('nav-link', '');
-		link.textContent = route;
+		link.textContent = view.link;
 		elems.push(link);
 	}
 });
 
-const viewElement = document.createElement('div');
-viewElement.id = 'view';
-elems.push(viewElement);
+elems.forEach(elem => parentElement.insertBefore(elem, parentElement.querySelector('#view')));
+
 
 // elems.push(document.querySelector('#counter'));
 
-
-elems.forEach(elem => parentElement.appendChild(elem));
 
 // setupCounter(document.querySelector('#counter'))
