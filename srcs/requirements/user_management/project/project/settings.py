60c942ad6eadc9bfab07a6df2f953904bf0e4548
Yoel RIDGWAY-LOPEZ <yoelridgway@gmail.com>
2024-01-18 16:03:15 +0100

    added django secret key

--- srcs/requirements/user_management/project/project/settings.py
+++ srcs/requirements/user_management/project/project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,7 @@ MEDIA_URL = '/media/'
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q(kyo2-+=u8uat#&h59tddpvut0b^pyd#gihr&t8+$bza^_z%t'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-q(kyo2-+=u8uat#&h59tddpvut0b^pyd#gihr&t8+$bza^_z%t')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
