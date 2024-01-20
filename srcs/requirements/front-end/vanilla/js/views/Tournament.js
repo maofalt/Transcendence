094f9365f5e126cc73d5d60e7c3144d4d95c5722
Amanda BRAVO <abysims31@gmail.com>
2024-01-20 19:58:57 +0100

    start the tournament front-end

--- srcs/requirements/front-end/vanilla/js/views/Tournament.js
+++ srcs/requirements/front-end/vanilla/js/views/Tournament.js
@@ -3,15 +3,43 @@ import AbstractView from "./AbstractView";
 export default class Tournament extends AbstractView {
 	constructor(element) {
 		super(element);
+		this.element = element; 
 	}
 
+	createElement(tag, attributes, content) {
+        const element = document.createElement(tag);
+		// const tournamentElement = document.getElementById('tournamentContainer');
+		// const tournamentView = new Tournament(tournamentElement);
+
+        // Ajout des attributs à l'élément
+        if (attributes) {
+            Object.keys(attributes).forEach(key => {
+                element.setAttribute(key, attributes[key]);
+            });
+        }
+
+        // Ajout du contenu à l'élément
+        if (content) {
+            element.innerHTML = content;
+        }
+
+        return element;
+    }
+
+	// Fonction pour obtenir le HTML
 	async getHtml() {
-		await new Promise(resolve => setTimeout(resolve, 500)); // Wait for 3 seconds
-		return `
-			<div class="card">
-				<h1>Tournament</h1>
-			</div>
-		`;
-	}
+        const tournamentContainer = this.createElement('div', { id: 'tournamentContainer' });
 
+        const titleElement = this.createElement('h1', null, 'Tournament');
+		const deleteButton = this.createElement('a', { id: 'deleteButton', class: 'deleteButton', href: 'https://google.com'}, "delete");
+
+        // Ajout des éléments créés au conteneur
+        tournamentContainer.appendChild(titleElement);
+		tournamentContainer.appendChild(deleteButton);
+
+        const htmlContent = tournamentContainer.innerHTML;
+
+        // Retour du HTML
+        return htmlContent;
+	}
 }
--- srcs/requirements/tournament_service/scripts/entrypoint.sh
+++ srcs/requirements/tournament_service/scripts/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-while ! nc -z tounrament_db 5432; do
+while ! nc -z tournament_db 5432; do
   sleep 1
 done
 
