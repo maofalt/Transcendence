e50805dc52788cb4fd06d1b09cc00673efe19399
motero <motero@student.42.fr>
2024-01-31 20:34:36 +0100

    ecnapsulating  bootstrap

--- srcs/requirements/front-end/vanilla/js/components/DynamicTable.js
+++ srcs/requirements/front-end/vanilla/js/components/DynamicTable.js
@@ -8,29 +8,29 @@ class DynamicTable {
      * @function loadBootstrap
      * @returns {void}
      */
-    loadBootstrap() {
-        const loadBootstrap = 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
-        const link = document.createElement('link');
-        link.href = loadBootstrap;
-        link.rel = 'stylesheet';
-        link.id  = 'bootstrap-css';
-        document.head.appendChild(link);
-    }
+    // loadBootstrap() {
+    //     const loadBootstrap = 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
+    //     const link = document.createElement('link');
+    //     link.href = loadBootstrap;
+    //     link.rel = 'stylesheet';
+    //     link.id  = 'bootstrap-css';
+    //     document.head.appendChild(link);
+    // }
 
-    unloadBootstrap() {
-        const bootstrapLink = document.getElementById('bootstrap-css');
-        if (bootstrapLink) {
-            //bootstrapLink.head.removeChild(bootstrapLink);
-            document.head.removeChild(bootstrapLink);
-        }
-    }
+    // unloadBootstrap() {
+    //     const bootstrapLink = document.getElementById('bootstrap-css');
+    //     if (bootstrapLink) {
+    //         //bootstrapLink.head.removeChild(bootstrapLink);
+    //         document.head.removeChild(bootstrapLink);
+    //     }
+    // }
 
     render() {
-        this.loadBootstrap();
+        //this.loadBootstrap();
 
         //Crea tea simple Bootrrape-syled element
         const container = document.createElement('div');
-        container.className = 'boostrap-container';
+        container.className = 'dynamic-table';
         container.innerHTML = '<div class="alert alert-info">Dynamic Table Component</div';
         
         return container;
