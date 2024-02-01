097a8f285ee6a5d8065fbd7644bc03456f382ef1
motero <motero@student.42.fr>
2024-02-01 17:02:20 +0100

    encpsualte css wiht bootstrap using WebComponent

--- srcs/requirements/front-end/vanilla/js/components/DynamicTable.js
+++ srcs/requirements/front-end/vanilla/js/components/DynamicTable.js
@@ -1,44 +1,17 @@
-class DynamicTable {
+class DynamicTable extends HTMLElement {
     constructor() {
-        // TODO
+        super();
+        this.attachShadow({ mode: 'open'});
     }
     
-    /**
-     * Loads the Bootstrap CSS file dynamically by creating a link element and appending it to the document head.
-     * @function loadBootstrap
-     * @returns {void}
-     */
-    // loadBootstrap() {
-    //     const loadBootstrap = 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
-    //     const link = document.createElement('link');
-    //     link.href = loadBootstrap;
-    //     link.rel = 'stylesheet';
-    //     link.id  = 'bootstrap-css';
-    //     document.head.appendChild(link);
-    // }
-
-    // unloadBootstrap() {
-    //     const bootstrapLink = document.getElementById('bootstrap-css');
-    //     if (bootstrapLink) {
-    //         //bootstrapLink.head.removeChild(bootstrapLink);
-    //         document.head.removeChild(bootstrapLink);
-    //     }
-    // }
-
-    render() {
-        //this.loadBootstrap();
-
-        //Crea tea simple Bootrrape-syled element
-        const container = document.createElement('div');
-        container.className = 'dynamic-table';
-        container.innerHTML = '<div class="alert alert-info">Dynamic Table Component</div';
-        
-        return container;
+    connectedCallback() {
+        this.shadowRoot.innerHTML = `
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <div class="alert alert-info">Dynamic Table Component</div>
+        `;
     }
 
-    destroy() {
-        this.unloadBootstrap();
-    }
 }
 
+customElements.define('dynamic-table', DynamicTable);
 export default DynamicTable;
--- srcs/requirements/front-end/vanilla/js/views/Tournament.js
+++ srcs/requirements/front-end/vanilla/js/views/Tournament.js
@@ -2,28 +2,14 @@ import AbstractView from "./AbstractView";
 import DynamicTable from "../components/DynamicTable";
 
 export default class Tournament extends AbstractView {
-	constructor(element) {
-		super(element);
-		this.dynamicTable = new DynamicTable();
-	}
 
 	async getHtml() {
 		await new Promise(resolve => setTimeout(resolve, 500)); // Wait for 3 seconds
 		return `
 			<div class="card">
 				<h1>Tournament</h1>
-				<div id="dynamic-table-container"></div>
+				<dynamic-table></dynamic-table>
 			</div>
 		`;
 	}
-
-	async init() {
-		document.getElementById('dynamic-table-container').appendChild(this.dynamicTable.render());
-	}
-
-	async destroy() {
-		if (this.dynamicTable)
-			this.dynamicTable.destroy();
-	}
-
 }
