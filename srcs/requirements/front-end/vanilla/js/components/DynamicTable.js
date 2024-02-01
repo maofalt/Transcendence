0debfd2b1910c40a31f43d344087a640af67a1e6
motero <motero@student.42.fr>
2024-02-01 20:26:20 +0100

    Dynamci table almost done

--- srcs/requirements/front-end/vanilla/js/components/DynamicTable.js
+++ srcs/requirements/front-end/vanilla/js/components/DynamicTable.js
@@ -1,16 +1,54 @@
+import ModalTable from "./ModalTable";
+
 class DynamicTable extends HTMLElement {
+    static get observedAttributes() {
+        return ['data-headers', 'data-rows'];
+    }
+    
     constructor() {
         super();
         this.attachShadow({ mode: 'open'});
-    }
-    
-    connectedCallback() {
         this.shadowRoot.innerHTML = `
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <div class="alert alert-info">Dynamic Table Component</div>
+            <link rel="stylesheet" href="../../css/DynamicTable.css">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+            <table class="table">
+                <thead><tr id="table-headers"></tr></thead>
+                <tbody id="table-body"></tbody>
+            </table>
         `;
     }
+    
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'data-headers') {
+            this.buildHeaders(JSON.parse(newValue));
+        } else if (name === 'data-rows') {
+            this.buildRows(JSON.parse(newValue));
+        }
+    }
 
+    buildHeaders(headers) {
+        const headersRow = this.shadowRoot.getElementById('table-headers');
+        headersRow.innerHTML = headers.map(header => `<th>${header}</th>`).join('');
+    }
+
+    buildRows(dataRows) {
+        const tbody = this.shadowRoot.querySelector('#table-body');
+        tbody.innerHTML = dataRows.map(row => {
+            // Assuming row is an object with keys matching headers
+            const rowHTML = Object.values(row).map(value => {
+                // Check for special types like avatars or badges
+                if (typeof value === 'object' && value.type === 'avatar') {
+                    return `<td><img src="${value.src}" class="rounded-circle" width="30" height="30"></td>`;
+                } else if (typeof value === 'object' && value.type === 'badge') {
+                    return `<td><span class="badge badge-${value.variant}">${value.text}</span></td>`;
+                } else if (typeof value === 'object' && value.type === 'button') {
+                    return `<td><button class="btn btn-${value.variant}">${value.text}</button></td>`;
+                }
+                return `<td>${value}</td>`;
+            }).join('');
+            return `<tr>${rowHTML}</tr>`;
+        }).join('');
+    }
 }
 
 customElements.define('dynamic-table', DynamicTable);
--- srcs/requirements/front-end/vanilla/js/views/Tournament.js
+++ srcs/requirements/front-end/vanilla/js/views/Tournament.js
@@ -3,6 +3,15 @@ import DynamicTable from "../components/DynamicTable";
 
 export default class Tournament extends AbstractView {
 
+	constructor() {
+		super();
+		this.headers = ['Tournament Name', 'Host', 'Number of Players', 'Time Remaining', 'Tournament Type', 'Registration Mode', 'Action'];
+		this.data= [
+			{tournamentName: 'Tournament 1', host: 'Host 1', numberOfPlayers: '2/4', timeRemaining: '2:00', tournamentType: 'Single Elimination', registrationMode: 'Open', action: 'Join'},
+			{tournamentName: 'Tournament 2', host: 'Host 2', numberOfPlayers: '5/4', timeRemaining: '2:00', tournamentType: 'Single Elimination', registrationMode: 'Open', action: 'Join'},
+		];
+	}
+
 	async getHtml() {
 		await new Promise(resolve => setTimeout(resolve, 500)); // Wait for 3 seconds
 		return `
@@ -11,5 +20,11 @@ export default class Tournament extends AbstractView {
 				<dynamic-table></dynamic-table>
 			</div>
 		`;
+	}
+
+	async init() {
+		const dynamicTable =  document.querySelector('dynamic-table');
+		dynamicTable.setAttribute('data-headers', JSON.stringify(this.headers));
+		dynamicTable.setAttribute('data-rows', JSON.stringify(this.data));
 	}
 }
