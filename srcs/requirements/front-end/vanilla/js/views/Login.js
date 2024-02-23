4a6b734132765436109b5e90f07480b11ae8971c
Yoel <yoelridgway@gmail.com>
2024-02-23 09:10:22 +0100

    centered form and added htmlToElement util function

--- srcs/requirements/front-end/vanilla/js/views/Login.js
+++ srcs/requirements/front-end/vanilla/js/views/Login.js
@@ -6,6 +6,9 @@ import { createElement } from "@utils/createElement";
 export default class Login extends AbstractView {
 	constructor(element) {
 		super(element);
+		this.state = {
+			
+		};
 	}
 
 	async getHtml() {
@@ -30,17 +33,47 @@ export default class Login extends AbstractView {
 		// 	console.error('Request Failed:', error);
 		// }
 
-		let signUpForm = `
-		<form class="sign-up-form">
+
+		let signUpForm = createElement('form', { class: 'sign-up-form' });
+		signUpForm.appendChild('input', { type: 'text', id: 'username', name: 'username', placeholder: 'Username' });
+		signUpForm.appendChild('input', { type: 'password', id: 'username', name: 'password', placeholder: 'Password' });
+		signUpForm.appendChild('input', { type: 'confirm_password', id: 'onfirm_passwordd', name: 'confirm_password', placeholder: 'Confirm Password' });
+		signUpForm.appendChild('input', { type: 'playername', id: 'playername', name: 'playername', placeholder: 'Name' });
+		signUpForm.appendChild('input', { type: 'signupEmail', id: 'signupEmail', name: 'signupEmail', placeholder: 'Email' });
+
+		// let signUpForm = `
+		// <form class="sign-up-form">
+		// 	<input type="text" id="username" name="username" placeholder="Username">
+		// 	<input type="password" id="password" name="password" placeholder="Password">
+		// 	<input type="confirm_password" id="confirm_password" name="confirm_password" placeholder="Confirm Password">
+		// 	<input type="playername" id="playername" name="playername" placeholder="Name">
+		// 	<input type="signupEmail" id="signupEmail" name="signupEmail" placeholder="Email Address">
+		// </form>
+		// <button class="sign-up-form">Sign Up</button>
+		// `;
+
+		// csrfmiddlewaretoken: BElrVdZWVMe739faEBIRobmbaZ9sNc6Q2gkE67MRDKScu3oLq56smwm9D3zT4nXk
+		// username: player1
+		// password: Passw0rd1
+		// confirm_password: Passw0rd1
+		// playername: Player1
+		// signupEmail: player@gmail.com
+		// access_code: 960029
+
+		let loginForm = `
+		<form class="login-form">
 			<input type="text" id="username" name="username" placeholder="Username">
 			<input type="password" id="password" name="password" placeholder="Password">
 		</form>
-		<button class="sign-up-form">Submit</button>
+		<button class="login-form">Submit</button>
 		`;
 
 		this.container = createElement('div', { id: 'loginContainer' });
-		let formContainer = createElement('div', { id: 'form-container' }, signUpForm);
+		let formContainer = createElement('div', { id: 'form-container' });
 		this.container.appendChild(formContainer);
+
+		this.formContainer.appendChild(signUpForm);
+		this.formContainer.appendChild(loginForm);
 		
 		
 		// let banana = document.createElement('a');
@@ -53,6 +86,13 @@ export default class Login extends AbstractView {
 	
 	async init() {
 		this.setupFormListener('sign-up-form', '/api/user_management/auth/login', 'application/x-www-form-urlencoded');
+	}
+
+	destroy() {
+		// Check if the submit button exists and if handleSubmit is bound
+		if (this.submitBtn && this.handleSubmit) {
+			this.submitBtn.removeEventListener('click', this.handleSubmit);
+		}
 	}
 
 	// Generalized setup form listener method
