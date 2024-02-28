d3aee65a1a691d72c5eb2b554a59f6cd692fa075
motero <motero@student.42.fr>
2024-02-28 21:16:00 +0100

    Refactor CreateTournament.js to include form submission event listener

--- srcs/requirements/front-end/vanilla/js/views/CreateTournament.js
+++ srcs/requirements/front-end/vanilla/js/views/CreateTournament.js
@@ -17,15 +17,22 @@ export default class CreateTournament extends AbstractView {
     }
     
     async init() {
-        let matchID = await this.createGame();
+        
+        document.getElementById('tournament-form').addEventListener('submit', async(event) => {
+            event.preventDefault();
+            let gameSettings = this.getGameSettingsFromForm();
+            console.log('Game Settings ANonymous:', gameSettings);
+        });
+  
+        let matchID = await this.createGame(gameSettings);
         console.log('Match ID:', matchID);
         this.game = new Game(matchID, 500, 830);
-        this.game.init();
+        await this.game.init();
     }
 
-    async createGame() {
+    async createGame(gameSettings) {
 		console.log('Create Game');
-		const gameSettings = this.getGameSettings();
+		//const gameSettings = this.getGameSettings();
 		try {
 			const response = await makeApiRequest('https://localhost:9443/game-logic/createMatch','POST',gameSettings);
 			console.log('Match created:', response.body);
@@ -49,7 +56,7 @@ export default class CreateTournament extends AbstractView {
                         <input type="text" 
                             id="tournament_name" 
                             name="tournament_name" 
-                            required 
+                            required
                             placeholder="Enter tournament name">
 
                         <label for="nbr_of_players">Number of Players:</label>
@@ -93,7 +100,7 @@ export default class CreateTournament extends AbstractView {
                       <label class="switch">
                         <input type="checkbox" id="game_type">
                         <span class="slider round"></span>
-                      </label>View
+                      </label>
 
                       <label for="nbr_of_players">Number of Players (2-8):</label>
                       <input type="range" id="nbr_of_players" name="nbr_of_players" min="2" max="8" value="5">
@@ -105,22 +112,31 @@ export default class CreateTournament extends AbstractView {
                       <input type="number" id="time_limit" name="time_limit" min="1" max="5" value="0">
 
                       <h3>Field Data</h3>
-                      <label for="walls_factor">Walls Factor (0-2):</label>
-                      <input type="range" id="walls_factor" name="walls_factor" min="0" max="2" step="0.1" value="0.7">
+                        <label for="walls_factor">Walls Factor (0-2):</label>
+                        <input type="range" id="walls_factor" name="walls_factor" min="0" max="2" step="0.1" value="0.7">
 
-                      <label for="size_of_goals">Size of Goals (15-30):</label>
-                      <input type="range" id="size_of_goals" name="size_of_goals" min="15" max="30" value="15">
+                        <label for="size_of_goals">Size of Goals (15-30):</label>
+                        <input type="range" id="size_of_goals" name="size_of_goals" min="15" max="30" value="15">
 
                       <h3>Paddles Data</h3>
-                      <label for="paddle_height">Paddle Height (1-12):</label>
-                      <input type="range" id="paddle_height" name="paddle_height" min="1" max="12" value="10">
-                      View
-                      <h3>Ball Data</h3>
-                      <label for="ball_speed">Ball Speed:</label>
-                      <input type="number" id="ball_speed" name="ball_speed" step="0.1" value="0.7">
+                        <label for="paddle_width">Paddle Width (1-12):</label>
+                        <input type="range" id="paddle_widht" name="paddle_widht" min="1" max="12" value="1">
 
-                      <label for="ball_radius">Ball Radius (0.5-7):</label>
-                      <input type="range" id="ball_radius" name="ball_radius" min="0.5" max="7" step="0.1" value="1">
+                        <label for="paddle_height">Paddle Height (1-12):</label>
+                        <input type="range" id="paddle_height" name="paddle_height" min="1" max="12" value="10">
+
+                        <label for="paddle_speed">Paddle Speed (1-12):</label>
+                        <input type="range" id="paddle_speed" name="paddle_speed" min="0" max="1" value="0.3">
+
+                      <h3>Ball Data</h3>
+                        <label for="ball_speed">Ball Speed:</label>
+                        <input type="number" id="ball_speed" name="ball_speed" step="0.1" value="0.7">
+
+                        <label for="ball_radius">Ball Radius (0.5-7):</label>
+                        <input type="range" id="ball_radius" name="ball_radius" min="0.5" max="7" step="0.1" value="1">
+
+                        <label for="ball_color">Ball Color:</label>
+                        <input type="color" id="ball_color" name="ball_color" value="#ff0000">
                     </form>
                 </div>                
                 <div class="game-showcase">
@@ -136,42 +152,87 @@ export default class CreateTournament extends AbstractView {
         return htmlElement.innerHTML;
     }
 
+    getGameSettingsFromForm() {
+      let gameSettings = {
+        "gamemodeData": {
+          "nbrOfPlayers": parseInt(document.getElementById('nbr_of_players').value, 10),
+          "nbrOfRounds": parseInt(document.getElementById('nbr_of_rounds').value, 10),
+          "timeLimit": parseInt(document.getElementById('time_limit').value, 10)
+        },
+        "fieldData": {
+          "wallsFactor": parseFloat(document.getElementById('walls_factor').value),
+          "sizeOfGoals": parseInt(document.getElementById('size_of_goals').value, 10)
+        },
+        "paddlesData": {
+          "width": parseInt(document.getElementById('paddle_width').value, 10),
+          "height": parseInt(document.getElementById('paddle_height').value, 10),
+          "speed": parseFloat(document.getElementById('paddle_speed').value)
+        },
+        "ballData": {
+          "speed": parseFloat(document.getElementById('ball_speed').value),
+          "radius": parseFloat(document.getElementById('ball_radius').value),
+          "color": document.getElementById('ball_color').value
+        },
+        "playersData": [
+          {
+            "accountID": "motero",
+            "color": "0x0000ff"
+          },
+        ]
+      }
+
+      // Add dummy players data in gameSetting depending on the number of players
+      let nbr_of_players = gameSettings.gamemodeData.nbrOfPlayers;
+      let dummyPlayeName = 'banana';
+      let dummyPlayerColor = '0x00ff00';
+      for (let i = 1; i < nbr_of_players; i++) {
+        let dummyPlayer = {
+          "accountID": dummyPlayeName + i,
+          "color": dummyPlayerColor
+        }
+        gameSettings.playersData.push(dummyPlayer);
+      }
+      
+      return gameSettings;
+    }
+
     getGameSettings() {
-		return {
-			"gamemodeData": {
-			  "nbrOfPlayers": 3,
-			  "nbrOfRounds": 1,
-			  "timeLimit": 0
-			},
-			"fieldData": {
-			  "wallsFactor": 1,
-			  "sizeOfGoals": 20
-			},
-			"paddlesData": {
-			  "width": 0,
-			  "height": 12,
-			  "speed": 0.5
-			},
-			"ballData": {
-			  "speed": 5,
-			  "radius": 100,
-			  "color": "0xf00fff"
-			},
-			"playersData": [
-			  {
-				"accountID": "motero",
-				"color": "0x0000ff"
-			  },
-			  {
-				"accountID": "tata2",
-				"color": "0x00ff00"
-			  },
-			  {
-				"accountID": "tata3",
-				"color": "0x00ff00"
-			  }
-			]
-		};
+		  let gameSettings = {
+		  	"gamemodeData": {
+		  	  "nbrOfPlayers": 3,
+		  	  "nbrOfRounds": 1,
+		  	  "timeLimit": 0
+		  	},
+		  	"fieldData": {
+		  	  "wallsFactor": 1,
+		  	  "sizeOfGoals": 20
+		  	},
+		  	"paddlesData": {
+		  	  "width": 0,
+		  	  "height": 12,
+		  	  "speed": 1
+		  	},
+		  	"ballData": {
+		  	  "speed": 0.5,
+		  	  "radius": 3,
+		  	  "color": "0xf00fff"
+		  	},
+		  	"playersData": [
+		  	  {
+		  		"accountID": "motero",
+		  		"color": "0x0000ff"
+		  	  },
+		  	  {
+		  		"accountID": "tata2",
+		  		"color": "0x00ff00"
+		  	  },
+		  	  {
+		  		"accountID": "tata3",
+		  		"color": "0x00ff00"
+		  	  }
+		  	]
+		  };
+      return gameSettings;
 	}
 
 }
