f0c976e390bfb98c13ba5e0b8c17e0690264747c
Jisu CHOI <cscard1002@gmail.com>
2024-03-06 18:27:39 +0100

    modified: name for 2fa options

--- srcs/requirements/user_management/project/account/forms.py
+++ srcs/requirements/user_management/project/account/forms.py
@@ -3,11 +3,11 @@ from .models import User
 from django.contrib.auth.forms import PasswordChangeForm
 
 class ProfileUpdateForm(forms.ModelForm):
-    TWO_FACTOR_CHOICES = [
+    TWO_FACTOR_OPTIONS = [
         ('sms', 'SMS'),
         ('email', 'Email'),
     ]
-    two_factor_method = forms.ChoiceField(choices=TWO_FACTOR_CHOICES, required=False)
+    two_factor_method = forms.ChoiceField(choices=TWO_FACTOR_OPTIONS, required=False)
 
     class Meta:
         model = User
