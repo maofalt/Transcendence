dea1067f03a6804113d963551942e07a57b1fcc8
Yoel <yoelridgway@gmail.com>
2024-03-11 12:01:34 +0100

    added signup submiting functionality

--- srcs/requirements/front-end/vanilla/js/views/Signup.js
+++ srcs/requirements/front-end/vanilla/js/views/Signup.js
@@ -44,15 +44,19 @@ export default class Signup extends AbstractComponent {
 		let usernameInput = new InputField({content: "Username", name: "username", type: "text"});
 		let passwordInput = new InputField({content: "Password", name: "password", type: "password"});
 		let confirmPasswordInput = new InputField({content: "Confirm Password", name: "confirm_password", type: "password"});
+		let playerNameInput = new InputField({content: "Player Name", name: "playername", type: "text"});
 		let emailInput = new InputField({content: "Email", name: "email", type: "text"});
 		let sendCodeToEmail = new CustomButton({content: "Send Code to Email", action: true, style: {margin: "15px 0px 0px 0px"}});
+		let accessCodeInput = new InputField({content: "Access Code", name: "access_code", type: "text"});
 		let signUpButton = new CustomButton({content: "Sign Up", action: true});
 
 		pannel.shadowRoot.appendChild(usernameInput);
 		pannel.shadowRoot.appendChild(passwordInput);
 		pannel.shadowRoot.appendChild(confirmPasswordInput);
+		pannel.shadowRoot.appendChild(playerNameInput);
 		pannel.shadowRoot.appendChild(emailInput);
 		pannel.shadowRoot.appendChild(sendCodeToEmail);
+		pannel.shadowRoot.appendChild(accessCodeInput);
 		pannel.shadowRoot.appendChild(signUpButton);
 
 		this.shadowRoot.appendChild(pannel);
@@ -61,8 +65,59 @@ export default class Signup extends AbstractComponent {
 		
 		passwordInput.oninput = (e) => this.checkPassword(e, passwordInput);
 		confirmPasswordInput.oninput = (e) => this.checkPasswordMatch(e, passwordInput, confirmPasswordInput);
-		// signUpButton.onclick = (e) => this.submitSignup(e);
 
+		signUpButton.onclick = (e) => this.submitSignup(e, {
+			username: usernameInput.getValue(),
+			password: passwordInput.getValue(),
+			confirm_password: confirmPasswordInput.getValue(),
+			playername: playerNameInput.getValue(),
+			signupEmail: emailInput.getValue(),
+			access_code: accessCodeInput.getValue()
+		});
+	}
+
+	// submitLoginForm = (e, formData) => {
+	submitSignup = (e, formData) => {
+		e && e.preventDefault();
+		
+		console.log('submitSignup');
+		console.log('values:', formData);
+
+		easyFetch('/api/user_management/auth/signup', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				'X-CSRFToken': getCookie('csrftoken')
+			},
+			body: new URLSearchParams(formData)
+		})
+		.then(res => {
+			let response = res.response;
+			let body = res.body;
+
+			if (!response || !body) {
+				alert('Request Failed');
+				return ;
+			}
+
+			if (response.status === 400) {
+				alert('Invalid signup data');
+				return ;
+			}
+	
+			if (!response.ok) {
+				alert('Response Error:', body.error || JSON.stringify(body));
+				return ;
+			}
+
+			if (response.status === 200 && body.success === true) {
+				alert('Login successful:', body.message || JSON.stringify(body));
+				Router.navigateTo("/");
+			}
+		})
+		.catch(error => {
+			console.error('Request Failed:', error);
+		});
 	}
 
 	checkPassword = (e, passwordInput) => {
@@ -130,11 +185,6 @@ export default class Signup extends AbstractComponent {
 		.catch(error => {
 			console.error('Request Failed:', error);
 		});
-	}
-
-	submitSignup = (e) => {
-		e.preventDefault();
-		console.log('submitSignup');
 	}
 
 	cancelSignup = (e) => {
