25d4dfed0e05a97b4ec234ddc5eaa0a9312f92c0
Yoel <yoelridgway@gmail.com>
2024-03-14 14:04:13 +0100

    fixed some css heights and scrolling problems

--- srcs/requirements/front-end/vanilla/js/utils/displayPopup.js
+++ srcs/requirements/front-end/vanilla/js/utils/displayPopup.js
@@ -4,5 +4,4 @@ export const displayPopup = () => {
 	let info = document.querySelector("#info");
 	info.style.display = "block";
 	// let info = new InfoPopup({content: "You have successfully signed up!"});
-	
 }
