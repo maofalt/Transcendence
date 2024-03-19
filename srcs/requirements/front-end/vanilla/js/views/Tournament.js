a2118f29a9d4c32f81d341153b7c4cc81251a72a
motero <motero@student.42.fr>
2024-03-19 21:17:38 +0100

    tournament now shows the real nbr of players in tournament and Total. As well models is corrected

--- srcs/requirements/front-end/vanilla/js/views/Tournament.js
+++ srcs/requirements/front-end/vanilla/js/views/Tournament.js
@@ -87,9 +87,9 @@ export default class Tournament extends AbstractView {
 				const tournamentNameElement = tournamentTable.createStyledHTMLObject('div', tournament.tournament_name, Styles.tournamentName);
 				const hostElement = tournamentTable.createStyledHTMLObject('div', `Host ${tournament.host_id}`, Styles.host);
 				//64
-				const numberOfPlayersElement = tournamentTable.createStyledHTMLObject('div', `0/${tournament.nbr_of_player_total}`, {}); 
+				const numberOfPlayersElement = tournamentTable.createStyledHTMLObject('div', `${tournament.joined}/${tournament.nbr_of_player_total}`, {}); 
 				const timeRemainingElement = tournamentTable.createStyledHTMLObject('div', '2:00', {}); // Placeholder for time remaining
-				const tournamentTypeElement = tournamentTable.createStyledHTMLObject('div', tournament.tournament_type === 1 ? 'Single Elimination' : 'Other Type', {});
+				const tournamentTypeElement = tournamentTable.createStyledHTMLObject('div', `${tournament.tournament_type}`);
 				const registrationModeElement = tournamentTable.createStyledHTMLObject('div', tournament.registration === 1 ? 'Open' : 'Invitational', {});
 				const joinButtonElement = document.createElement('button');
 				joinButtonElement.textContent = 'Join';
