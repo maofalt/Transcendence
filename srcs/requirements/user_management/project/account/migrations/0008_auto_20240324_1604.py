a43cf04701bcd2d5d9c18658cf44c71a16837bd8
motero <motero@student.42.fr>
2024-03-24 18:58:55 +0100

    migration to create  10 basic user accounts

--- srcs/requirements/user_management/project/account/migrations/0008_auto_20240324_1604.py
+++ srcs/requirements/user_management/project/account/migrations/0008_auto_20240324_1604.py
@@ -3,7 +3,6 @@
 from django.db import migrations
 from django.contrib.auth import get_user_model
 
-
 def create_users(apps, schema_editor):
     User = get_user_model()
     usernames = ['motero', 'jisu', 'znogueira', 'amanda', 'yoel']
