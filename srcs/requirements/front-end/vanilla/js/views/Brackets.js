186c4066fc87a3ded9caf78d73e31ddc79b8a8c5
Yoel <yoelridgway@gmail.com>
2024-04-01 19:40:58 +0200

    fixed the small syntax errors

--- srcs/requirements/front-end/vanilla/js/views/Brackets.js
+++ srcs/requirements/front-end/vanilla/js/views/Brackets.js
@@ -34,24 +34,26 @@ export default class Brackets extends AbstractComponent {
 			let response = res.response;
 			let body = res.body;
 
-			if (!response || !body) {
-				throw new Error('Empty Response');
-			} else if (!response.ok) {
-				throw new Error(body.error || JSON.stringify(body));
-			} else if (response.status === 200) {
-					// displayPopup(body.message || JSON.stringify(body), 'success');
-				} else {
-					displayPopup(body.error || JSON.stringify(body), 'error');
-				}
-			} else {
-				displayPopup(body.error || JSON.stringify(body), 'error');
-			}
+		// 	if (!response || !body) {
+		// 		throw new Error('Empty Response');
+		// 	} else if (!response.ok) {
+		// 		throw new Error(body.error || JSON.stringify(body));
+		// 	} else if (response.status === 200) {
+		// 			// displayPopup(body.message || JSON.stringify(body), 'success');
+		// 		} else {
+		// 			displayPopup(body.error || JSON.stringify(body), 'error');
+		// 		}
+		// 	} else {
+		// 		displayPopup(body.error || JSON.stringify(body), 'error');
+		// 	}
 		})
 		.catch(error => {
 			displayPopup(`Request Failed: ${error}`, 'error');
 		});
         this.shadowRoot.appendChild(banana);
-    }
+	}
+
+}
 
 
 customElements.define('tournament-brackets', Brackets);
