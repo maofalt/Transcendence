8cefa6d1b51c0af4e5273a3ac77e9a4fdab2d2ba
motero home <motero@student.42.fr>
2024-04-03 01:43:34 +0200

    fixed the avatar picture no showing

--- srcs/requirements/front-end/vanilla/js/components/HostAvatar.js
+++ srcs/requirements/front-end/vanilla/js/components/HostAvatar.js
@@ -7,7 +7,7 @@ class HostAvatar extends HTMLElement {
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
             <div class="host-container">
-                <div class="host-avatar" style=""></div>
+                <img class="host-avatar" style="">
                 <div class="host-name"><slot></slot></div>
             </div>
         `;
