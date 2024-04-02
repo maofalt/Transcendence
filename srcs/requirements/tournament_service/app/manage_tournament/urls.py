c8c4272506f998b6e33898c010057dc33b0c445c
coisu <cscard1002@gmail.com>
2024-04-02 20:25:51 +0200

    fixed: match generator

--- srcs/requirements/tournament_service/app/manage_tournament/urls.py
+++ srcs/requirements/tournament_service/app/manage_tournament/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
 # Tournament Progression:
     # GET /{id}/matches - Retrieve a list of matches for a tournament.
     # POST /{id}/matches - Create a new match within a tournament.
-    path('<int:tournament_id>/match-generator/', views.MatchGenerator.as_view(), name='match-generator'),
+    path('<int:id>/match-generator/', views.MatchGenerator.as_view(), name='match-generator'),
     path('<int:id>/matches/', views.TournamentMatchList.as_view(), name='tournament-matches'),
     # DELETE /tournament/{id}/matches/{matchId} - Cancel a scheduled match.
     path('<int:id>/matches/<int:match_id>/', views.TournamentMatchDetail.as_view(), name='tournament-match-detail'),
