94e862f4acb869654cf31e568919af2b3f1cfdc6
maofalt <maofalt@gmail.com>
2024-04-03 17:32:42 +0200

    component modify to any nbr of players, calling the image wihtout any  if: : :

--- srcs/requirements/front-end/vanilla/js/components/NumberOfPlayers.js
+++ srcs/requirements/front-end/vanilla/js/components/NumberOfPlayers.js
@@ -3,7 +3,7 @@ class NumberOfPlayers extends HTMLElement {
         super();
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.innerHTML = `
-            <link rel="stylesheet" href="../../css/HostAvatar.css">
+            <link rel="stylesheet" href="../../css/NumberOfPlayers.css">
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
             <img class="players-icon" style="">
@@ -28,7 +28,7 @@ class NumberOfPlayers extends HTMLElement {
             this.shadowRoot.querySelector('.players-icon').src = "";
             return;
         }
-        const url = `../js/assets/images/players_${nbrOfPlayers}.svg`;
+        const url = `../js/assets/images/${nbrOfPlayers}_players.svg`;
         this.shadowRoot.querySelector('.players-icon').src = url;
     }
 }
