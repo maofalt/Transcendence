fc73e0028c4055fd6eb1b78f20166abc33f64759
motero <motero@student.42.fr>
2024-04-08 19:50:47 +0200

    cleaned tournament a little bit

--- srcs/requirements/front-end/vanilla/js/views/Tournament.js
+++ srcs/requirements/front-end/vanilla/js/views/Tournament.js
@@ -8,6 +8,8 @@ import HostAvatar from '@components/HostAvatar';
 import NumberOfPlayers from '@components/NumberOfPlayers';
 import { makeApiRequest } from '@utils/makeApiRequest.js';
 import { navigateTo } from '@utils/Router.js';
+import displayPopup from '@utils/displayPopup';
+
 
 export default class Tournament extends AbstractView {
 
@@ -29,130 +31,36 @@ export default class Tournament extends AbstractView {
 						id="createTournamentButton"
 					</action-button>
 				</div>
-				<tournament-table id="tournamentTable"></tournament-table>
 			</div>
 		`;
 	}
 
 	async init() {
-		await this.getTournamentList();
 		
-		const tournamentTable =  document.querySelector('tournament-table');
-
+		const tournamentTable = await this.getTournamentList();
+		console.log("TOURNAMENT TABLE", tournamentTable);
+		//insert the table in the tournament div
 		const createTournamentButton = document.getElementById('createTournamentButton');
 		createTournamentButton.addEventListener('click', this.createTournament);
+		const tournamentDiv = document.querySelector('.tournament');
+		tournamentDiv.appendChild(tournamentTable);
+
 		
 	}
 
-	// async getTournamentList() { 
-	// 	try {
-	// 		const response = await makeApiRequest('/api/tournament/create-and-list/','GET');
-	// 		const tournaments = response.body;
-	// 		if (!tournaments) 
-	// 			throw new Error('Failed to get tournament list');
-	// 		// Transform the data
-	// 		if (tournaments.length === 0) {
-	// 			this.data = [];
-	// 			return;
-	// 		}
-
-	// 		const responseUser = await makeApiRequest('/api/user_management/auth/getUser', 'GET');
-	// 		const userName = responseUser.body.username;
-
-	// 		await tournamentTable.applyColumnStyles();
-	// 		let Styles = tournamentTable.columnStyles;
-			
-	// 		// Map API data to table rows and add them to the table
-	// 		tournaments.forEach(async (tournament) => {
-	// 			const response = await makeApiRequest(`/api/tournament/${tournament.id}/participants/`, 'GET');
-	// 			const participants = response.body;
-	// 			console.log("PARTICIPANTS", participants);
-	// 			if (participants.players_username.includes(userName)) {
-	// 				tournament.is_in_tournament = true;
-	// 			} else {
-	// 				tournament.is_in_tournament = false;
-	// 			}
-	// 		// TOURNAMENT STYLES APPLIED
-	// 			const tournamentNameElement = tournamentTable.createStyledHTMLObject('div', tournament.tournament_name, Styles.tournamentName);
-
-	// 		//TOURNAMENST HOST 
-	// 			//trying tor ecover the name id and the picture
-	// 			const hostName = tournament.host_name;
-	// 			const hostAvatarElement = document.createElement('host-avatar');
-	// 			hostAvatarElement.setAttribute('name', hostName);
-	// 			//APi call to recover host picture
-
-	// 			const hostElement = tournamentTable.createStyledHTMLObject('div', hostAvatarElement, Styles.host);
-	// 			//const hostElement = tournamentTable.createStyledHTMLObject('div', `${hostName}`, Styles.host);
-			
-	// 		//TOURNAMENT PLAYERS OER TOURNAMENT AND PLACE AVAILABLE	
-	// 			const numberOfPlayersElement = tournamentTable.createStyledHTMLObject('div', `${tournament.joined}/${tournament.nbr_of_player_total}`, Styles.nbrOfPlayersTournament);
-			
-	// 		// TOURNAMENT PLAYER  PER MATCH!!
-	// 			const componentNbrOfPlayers = document.createElement('number-of-players');
-	// 			componentNbrOfPlayers.setAttribute('nbrOfPlayers', tournament.nbr_of_player_match);	
-	// 			const numberOfPlayerPerMatch = tournamentTable.createStyledHTMLObject('div', componentNbrOfPlayers, Styles.nbrOfPlayersMatch);
-			
-	// 		//TOURNAMENT STATUS	
-	// 			const tournamentStatus = tournamentTable.createStyledHTMLObject('div', `${tournament.state}`, Styles.state);
-			
-	// 		//TOURNAMENT ACTION BUTTONS	
-	// 			//Join button on te action column to join corresponding tournament
-	// 			let buttonText = '';
-	// 			let buttonEvent = null;
-				
-	// 			if (hostName === userName && tournament.state === 'waiting') {
-	// 				console.log("TOURNAMENT IS WAITING", tournament);
-	// 				buttonText = 'Start';
-	// 				buttonEvent = async () => {
-	// 					const apiEndpoint = `/api/tournament/${tournament.id}/start/`;
-	// 					await makeApiRequest(apiEndpoint, 'POST');
-	// 				};
-	// 			} else if (tournament.is_in_tournament && tournament.state === 'started') {
-	// 				buttonText = 'Play';
-	// 				buttonEvent = () => navigateTo('/play?matchID=' + tournament.setting.id);
-	// 			} else if (tournament.is_in_tournament && tournament.state === 'waiting') {
-	// 				buttonText = 'Unjoin';
-	// 				// ADD UNJOIN ENDPOINT HERE
-	// 			} else {
-	// 				buttonText = 'Join';
-	// 				buttonEvent = async () => await this.joinTournament(tournament.id);
-	// 			}
-	// 			const actionButtonElement = tournamentTable.createStyledHTMLObject('button', buttonText, Styles.action);				
-	// 			if (buttonEvent) {
-	// 				actionButtonElement.onclick = buttonEvent;
-	// 			}
-
-	// 		//TOURNAMENT Details button to see the tournament state (either brackets and or results) opening th emodal
-	// 			const tournamentDetails = tournamentTable.createStyledHTMLObject('button', '👁️', Styles.details);
-	// 			tournamentDetails.addEventListener('click', () => {
-	// 				navigateTo(`/brackets?tournament=${tournament.id}`);
-	// 				// console.log(`SOMEONE IS WATCHING 👁️ 👁️`);
-	// 			});
-
-	// 			//Add the constructed row to the table
-	// 			tournamentTable.addRow([
-	// 				tournamentNameElement,
-	// 				hostElement,
-	// 				numberOfPlayersElement,
-	// 				numberOfPlayerPerMatch,
-	// 				tournamentStatus,
-	// 				actionButtonElement,
-	// 				tournamentDetails
-	// 			]);
-	// 		});
-
-	// 	} catch (error) {
-	// 		console.error('Failed to get tournament list:', error);
-	// 	}
-	// }
-
 	async getTournamentList() { 
+		//Create new table
+		console.log("GET TOURNAMENT LIST");
+		let tournamentTable = document.createElement('tournament-table');
+		
 		try {
 			const response = await makeApiRequest('/api/tournament/create-and-list/','GET');
 			let tournaments = response.body;
 			if (!tournaments) 
+			{	
+				displayPopup('Action failed:', 'Failed to get tournament list');
 				throw new Error('Failed to get tournament list');
+			}
 			// Transform the data
 			if (tournaments.length === 0) {
 				this.data = [];
@@ -171,14 +79,17 @@ export default class Tournament extends AbstractView {
 				if ((a.state === 'started' || a.state === 'ended') && b.state === 'waiting') return 1;
 				return 0;
 			});
-	
+			console.log("TOURNAMENTS", tournaments);
+			console.log("tournament length", tournaments.length);
 			// Map API data to table rows and add them to the table
 			for (let i = 0; i < tournaments.length; i++) {
+				console.log("i = ", i);
 				const tournament = tournaments[i];
 				try {
+					console.log( "TOURNAMENT", tournament)
 					const response = await makeApiRequest(`/api/tournament/${tournament.id}/participants/`, 'GET');
 					const participants = response.body;
-					console.log("PARTICIPANTS", participants);
+
 					if (participants.players_username.includes(userName)) {
 						tournament.is_in_tournament = true;
 					} else {
@@ -254,140 +165,65 @@ export default class Tournament extends AbstractView {
 					]);
 				} catch (error) {
 					console.error('Failed to get tournament list:', error);
+					displayPopup('Action failed:', error);
 				}
 			}
-	
+			
 		} catch (error) {
-			console.error('Failed to get tournament list:', error);
+			console.error('Error:', error);
+			
 		}
+		return tournamentTable;
 	}
 	
-
-	// async getTournamentList() { 
-	// 	try {
-	// 		const response = await makeApiRequest('/api/tournament/create-and-list/','GET');
-	// 		const tournaments = response.body;
-	// 		if (!tournaments) 
-	// 			throw new Error('Failed to get tournament list');
-	// 		// Transform the data
-	// 		if (tournaments.length === 0) {
-	// 			this.data = [];
-	// 			return;
-	// 		}
-	
-	// 		const responseUser = await makeApiRequest('/api/user_management/auth/getUser', 'GET');
-	// 		const userName = responseUser.body.username;
-	
-	// 		await tournamentTable.applyColumnStyles();
-	// 		let Styles = tournamentTable.columnStyles;
-			
-	// 		// Map API data to table rows and add them to the table
-	// 		for (let i = 0; i < tournaments.length; i++) {
-	// 			const tournament = tournaments[i];
-	// 			try {
-	// 				const response = await makeApiRequest(`/api/tournament/${tournament.id}/participants/`, 'GET');
-	// 				const participants = response.body;
-	// 				console.log("PARTICIPANTS", participants);
-	// 				if (participants.players_username.includes(userName)) {
-	// 					tournament.is_in_tournament = true;
-	// 				} else {
-	// 					tournament.is_in_tournament = false;
-	// 				}
-	// 				// TOURNAMENT STYLES APPLIED
-	// 				const tournamentNameElement = tournamentTable.createStyledHTMLObject('div', tournament.tournament_name, Styles.tournamentName);
-	
-	// 				// TOURNAMENST HOST 
-	// 				//trying tor ecover the name id and the picture
-	// 				const hostName = tournament.host_name;
-	// 				const hostAvatarElement = document.createElement('host-avatar');
-	// 				hostAvatarElement.setAttribute('name', hostName);
-	// 				//APi call to recover host picture
-	
-	// 				const hostElement = tournamentTable.createStyledHTMLObject('div', hostAvatarElement, Styles.host);
-	// 				//const hostElement = tournamentTable.createStyledHTMLObject('div', `${hostName}`, Styles.host);
-	
-	// 				// TOURNAMENT PLAYERS OER TOURNAMENT AND PLACE AVAILABLE    
-	// 				const numberOfPlayersElement = tournamentTable.createStyledHTMLObject('div', `${tournament.joined}/${tournament.nbr_of_player_total}`, Styles.nbrOfPlayersTournament);
-	
-	// 				// TOURNAMENT PLAYER  PER MATCH!!
-	// 				const componentNbrOfPlayers = document.createElement('number-of-players');
-	// 				componentNbrOfPlayers.setAttribute('nbrOfPlayers', tournament.nbr_of_player_match);
-	// 				const numberOfPlayerPerMatch = tournamentTable.createStyledHTMLObject('div', componentNbrOfPlayers, Styles.nbrOfPlayersMatch);
-	
-	// 				// TOURNAMENT STATUS    
-	// 				const tournamentStatus = tournamentTable.createStyledHTMLObject('div', `${tournament.state}`, Styles.state);
-	
-	// 				// TOURNAMENT ACTION BUTTONS    
-	// 				//Join button on te action column to join corresponding tournament
-	// 				let buttonText = '';
-	// 				let buttonEvent = null;
-	
-	// 				if (hostName === userName && tournament.state === 'waiting') {
-	// 					console.log("TOURNAMENT IS WAITING", tournament);
-	// 					buttonText = 'Start';
-	// 					buttonEvent = async () => {
-	// 						const apiEndpoint = `/api/tournament/${tournament.id}/start/`;
-	// 						await makeApiRequest(apiEndpoint, 'POST');
-	// 					};
-	// 				} else if (tournament.is_in_tournament && tournament.state === 'started') {
-	// 					buttonText = 'Play';
-	// 					buttonEvent = () => navigateTo('/play?matchID=' + tournament.setting.id);
-	// 				} else if (tournament.is_in_tournament && tournament.state === 'waiting') {
-	// 					buttonText = 'Unjoin';
-	// 					// ADD UNJOIN ENDPOINT HERE
-	// 				} else {
-	// 					buttonText = 'Join';
-	// 					buttonEvent = async () => await this.joinTournament(tournament.id);
-	// 				}
-	// 				const actionButtonElement = tournamentTable.createStyledHTMLObject('button', buttonText, Styles.action);
-	// 				if (buttonEvent) {
-	// 					actionButtonElement.onclick = buttonEvent;
-	// 				}
-	
-	// 				// TOURNAMENT Details button to see the tournament state (either brackets and or results) opening th emodal
-	// 				const tournamentDetails = tournamentTable.createStyledHTMLObject('button', '👁️', Styles.details);
-	// 				tournamentDetails.addEventListener('click', () => {
-	// 					navigateTo(`/brackets?tournament=${tournament.id}`);
-	// 					// console.log(`SOMEONE IS WATCHING 👁️ 👁️`);
-	// 				});
-	
-	// 				// Add the constructed row to the table
-	// 				tournamentTable.addRow([
-	// 					tournamentNameElement,
-	// 					hostElement,
-	// 					numberOfPlayersElement,
-	// 					numberOfPlayerPerMatch,
-	// 					tournamentStatus,
-	// 					actionButtonElement,
-	// 					tournamentDetails
-	// 				]);
-	// 			} catch (error) {
-	// 				console.error('Failed to get tournament list:', error);
-	// 			}
-	// 		}
-	
-	// 	} catch (error) {
-	// 		console.error('Failed to get tournament list:', error);
-	// 	}
-	// }
-
-
 	async createTournament() {
 		navigateTo('/create-tournament');
 	}
 
-	async joinTournament(tournamentID) {
+	// async joinTournament(tournamentID) {
 	
+	// 	try {
+	// 		const responseUser = await makeApiRequest(`/api/user_management/auth/getUser`,'GET');
+	// 		const userID = responseUser.body.user_id;
+	// 		const apiEndpoint = `/api/tournament/add-player/${tournamentID}/${userID}/`;
+	// 		const response = await makeApiRequest(apiEndpoint,'POST');
+	// 		//navigateTo('/play?matchID=' + response.body.matchID);
+	// 	} catch (error) {
+	// 		console.error('Failed to join tournament:', error);
+	// 	}
+	// }
+
+	async joinTournament(tournamentID) {
+		let userID;
+	
+		// Attempt to fetch user details
 		try {
-			const responseUser = await makeApiRequest(`/api/user_management/auth/getUser`,'GET');
-			const userID = responseUser.body.user_id;
-			const apiEndpoint = `/api/tournament/add-player/${tournamentID}/${userID}/`;
-			const response = await makeApiRequest(apiEndpoint,'POST');
-			//navigateTo('/play?matchID=' + response.body.matchID);
+			const responseUser = await makeApiRequest(`/api/user_management/auth/getUser`, 'GET');
+			if (responseUser.status >= 400) { 
+				throw new Error('Failed to fetch user details.');
+			}
+			userID = responseUser.body.user_id;
 		} catch (error) {
-			console.error('Failed to join tournament:', error);
+			console.error('Error fetching user details:', error);
+			displayPopup(error.message, 'error');
+			return;
+		}
+		// Attempt to add player to tournament
+		try {
+			const apiEndpoint = `/api/tournament/add-player/${tournamentID}/${userID}/`;
+			const response = await makeApiRequest(apiEndpoint, 'POST');
+			console.log("RESPONSE", response);
+
+			if (response.status >= 400) { 
+				throw new Error('Failed to join tournament: ' + response.body);
+			}
+			displayPopup('Successfully joined the tournament!', 'success');
+			//navigateTo('/play?matchID=' + response.body.matchID); // Assuming navigation is desired on success
+		} catch (error) {
+			console.error('Error joining tournament:', error);
+			displayPopup(error.message, 'error');
 		}
 	}
-
+	
 
 }
