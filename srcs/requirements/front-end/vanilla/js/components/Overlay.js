77213503b6fac32e919e7703fa508762d0ed13fc
motero <motero@student.42.fr>
2024-04-14 17:24:08 +0200

    Creation of over;ay compoenent logic

---
+++ srcs/requirements/front-end/vanilla/js/components/Overlay.js
@@ -0,0 +1,35 @@
+class Overlay extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `
+            <link rel="stylesheet" href="../../css/Overlay.css">
+            <div class="overlay">
+                <span class="closebtn">&times;</span>
+                <div class="overlay-content">
+                    <slot></slot>
+                </div>
+            </div>
+        `;
+
+        this.overlay = this.shadowRoot.querySelector('.overlay');
+        this.closeButton = this.shadowRoot.querySelector('.closebtn');
+        this.closeButton.addEventListener('click', () => this.hide());
+    }
+
+    connectedCallback() {
+        if (!this.hasAttribute('role'))
+            this.setAttribute('role', 'dialog');
+    }
+
+    show() {
+        this.overlay.style.display = 'block';
+    }
+
+    hide() {
+        this.overlay.style.display = 'none';
+    }
+}
+
+customElements.define('custom-overlay', OverlayComponent);
+export default Overlay;
