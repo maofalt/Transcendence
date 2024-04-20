875ec3e813b49844117834c19e081f740cdb313f
Jisu CHOI <cscard1002@gmail.com>
2024-04-20 17:04:12 +0200

    protect for joining after tournament has been started

--- srcs/requirements/tournament_service/app/manage_tournament/views.py
+++ srcs/requirements/tournament_service/app/manage_tournament/views.py
@@ -175,6 +175,8 @@ class JoinTournament(generics.ListCreateAPIView):
             return JsonResponse({'error': 'Tournament not found'}, status=404)
         if tournament.is_full():
             return JsonResponse({'message': 'Tournament is full'}, status=status.HTTP_400_BAD_REQUEST)
+        if tournament.state != 'waiting':
+            return JsonResponse({'message': 'Tournament is not in registration state'}, status=status.HTTP_400_BAD_REQUEST)
 
         player, created = Player.objects.get_or_create(id=uid, username=username) # created wiil return False if the player already exists
         if tournament.players.filter(id=player.id).exists():
